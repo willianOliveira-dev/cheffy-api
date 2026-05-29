9aec29001c7fd30ee28f82f9fa1a571aac494ee7
Willian Oliveira <willian.dev.tech@gmail.com>
2026-05-29 13:36:56 -0300

    fix(auth): add storeStateStrategy to resolve state_mismatch issue

--- src/lib/auth/auth.lib.ts
+++ src/lib/auth/auth.lib.ts
@@ -6,25 +6,29 @@ import { env } from '@/config/env.js';
 import { prisma } from '../db/prisma.js';
 
 export const auth = betterAuth({
-  baseURL: env.BETTER_AUTH_URL,
-  secret: env.BETTER_AUTH_SECRET,
-  trustedOrigins: env.ALLOWED_ORIGINS,
-  advanced: {
-    trustedProxyHeaders: true,
-    defaultCookieAttributes: {
-      sameSite: "none",
-      secure: true,
-      partitioned: true,
+    baseURL: env.BETTER_AUTH_URL,
+    secret: env.BETTER_AUTH_SECRET,
+    trustedOrigins: env.ALLOWED_ORIGINS,
+    account: {
+        storeStateStrategy: 'database',
     },
-    disableOriginCheck: env.NODE_ENV !== "production",
-    disableCSRFCheck: env.NODE_ENV !== "production",
-  },
-  socialProviders: {
-    google: {
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
+
+    advanced: {
+        trustedProxyHeaders: true,
+        defaultCookieAttributes: {
+            sameSite: 'none',
+            secure: true,
+            partitioned: true,
+        },
+        disableOriginCheck: env.NODE_ENV !== 'production',
+        disableCSRFCheck: env.NODE_ENV !== 'production',
     },
-  },
-  database: prismaAdapter(prisma, { provider: "postgresql" }),
-  plugins: [openAPI(), localization({ defaultLocale: "pt-BR" })],
+    socialProviders: {
+        google: {
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+        },
+    },
+    database: prismaAdapter(prisma, { provider: 'postgresql' }),
+    plugins: [openAPI(), localization({ defaultLocale: 'pt-BR' })],
 });
